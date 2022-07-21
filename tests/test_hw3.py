34fba14ca12149fcc67532dc5bbc3ca539a45fa2
Ido <ido.had7@gmail.com>
2022-07-21 14:13:46 +0300

    home work 3 tests file final version 21.07.22

--- tests/test_hw3.py
+++ tests/test_hw3.py
@@ -1,10 +1,10 @@
 import hw3
 import pytest
 import logging
-import sys
 
 
-@pytest.fixture
+
+@pytest.fixture(scope="function")
 def data_load():
     return {3322117:{"name": "Tal", "sex": "male", "age": 12},332117:{"name": "Tl", "sex": "male", "age": 1},
               176864301:{"sex": "female", "age": 57, "height": 1.65,"name": "Anat"},
@@ -12,7 +12,8 @@ def data_load():
 
 def test_split_male_female(data_load):
     male,female=hw3.split_male_female(data_load)
-    logging.info(male)
+    logging.debug("all members before actions"+str(data_load))
+    logging.info("male members after function call:"+str(male))
     lst=[]
     for key,values in male.items():
         lst.append(values["sex"])
@@ -21,6 +22,8 @@ def test_split_male_female(data_load):
         lst2.append(values["sex"])
     assert ("male") in lst
     assert ("female") not in lst
+    logging.debug("moving on to female member tests--------------------")
+    logging.info("female members:"+str(female))
     assert len(lst)==2
     assert len(lst2)==2
     assert ("male") not in lst2
@@ -31,10 +34,23 @@ def test_mean_median(capsys,data_load):
     hw3.find_median_average(data_load)
     captured=capsys.readouterr()
     str=captured.out
+    logging.info("result after calling find_mean_median:"+captured.out)
     assert "18.75" in str
     assert "8.5" in str
     #mean=18.75 median=8.5
 
 def test_print_above(capsys,data_load):
-    pass
+    hw3.print_values_above(data_load,56)
+    captured=capsys.readouterr()
+    str=captured.out
+    logging.info("result after calling print_values_above(56):"+str)
+    assert "176864301 {'sex': 'female', 'age': 57, 'height': 1.65, 'name': 'Anat'}" in str
+    hw3.print_values_above(data_load,100)
+    captured=capsys.readouterr()
+    str=captured.out
+    logging.info("result after calling print_values_above(100):"+str)
+    assert len(str)==0
+
+
+
 
